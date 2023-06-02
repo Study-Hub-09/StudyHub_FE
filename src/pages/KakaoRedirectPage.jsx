2babf06085e3fefd69e87831be710ec4ea067bfb
Hanbit Kang <hanbitkang95@gmail.com>
2023-06-02 16:36:29 +0900

    refactor: kakaoredirectpage assignment

--- src/pages/KakaoRedirectPage.jsx
+++ src/pages/KakaoRedirectPage.jsx
@@ -14,43 +14,43 @@ function KakaoRedirectPage({ width }) {
   const navigate = useNavigate();
   const code = new URL(window.location.href).searchParams.get('code');
 
-  // useEffect(() => {
-  //   const accessToken = getCookie('AccessToken');
-  //   if (accessToken) {
-  //     alert('이미 로그인된 유저입니다');
-  //     navigate('/main');
-  //   } else {
-  //     instance
-  //       .get(`/api/members/kakao/callback?code=${code}`)
-  //       .then((response) => {
-  //         console.log(response);
-  //         const statusCode = response.status;
-  //         const accessToken = response.headers.get('access_token').split(' ')[1];
-  //         const refreshToken = response.headers.get('refresh_token').split(' ')[1];
-  //         setCookie('AccessToken', accessToken, { path: '/' });
-  //         setCookie('RefreshToken', refreshToken, { path: '/' });
+  useEffect(() => {
+    const accessToken = getCookie('AccessToken');
+    if (accessToken) {
+      alert('이미 로그인된 유저입니다');
+      navigate('/main');
+    } else {
+      instance
+        .get(`/api/members/kakao/callback?code=${code}`)
+        .then((response) => {
+          console.log(response);
+          const statusCode = response.status;
+          const accessToken = response.headers.get('access_token').split(' ')[1];
+          const refreshToken = response.headers.get('refresh_token').split(' ')[1];
+          setCookie('AccessToken', accessToken, { path: '/' });
+          setCookie('RefreshToken', refreshToken, { path: '/' });
 
-  //         if (statusCode === 200) {
-  //           alert('로그인 성공!');
-  //           navigate('/');
-  //         } else {
-  //           alert('로그인 실패!');
-  //           navigate('/members/login');
-  //         }
-  //       })
-  //       .catch((error) => {
-  //         console.log(error);
-  //         const statusCode = error.response?.status;
-  //         if (statusCode === 401) {
-  //           alert('이미 로그인된 유저입니다.');
-  //           navigate('/main');
-  //         } else {
-  //           alert('로그인 실패!');
-  //           navigate('/members/login');
-  //         }
-  //       });
-  //   }
-  // }, []);
+          if (statusCode === 200) {
+            alert('로그인 성공!');
+            navigate('/');
+          } else {
+            alert('로그인 실패!');
+            navigate('/members/login');
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          const statusCode = error.response?.status;
+          if (statusCode === 401) {
+            alert('이미 로그인된 유저입니다.');
+            navigate('/main');
+          } else {
+            alert('로그인 실패!');
+            navigate('/members/login');
+          }
+        });
+    }
+  }, []);
 
   return (
     <StKakaoRedirectContainer>
