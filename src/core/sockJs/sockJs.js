1dac23d155acf5ef5e6eeadf05ef00be18b3b7d6
Hanbit Kang <hanbitkang95@gmail.com>
2023-06-12 19:30:59 +0900

    feat : chatting

--- src/core/sockJs/sockJs.js
+++ src/core/sockJs/sockJs.js
@@ -3,20 +3,35 @@ import { Stomp } from '@stomp/stompjs';
 import { getCookie } from '../../Cookies/Cookies';
 
 let stompClient = null;
+const accessToken = getCookie('AccessToken');
+const refreshToken = getCookie('RefreshToken');
 
-const connectClient = () => {
-  const socket = new SockJs(`${process.env.REACT_APP_SERVER_URL}/ws-edit`);
+const connectClient = (sessionId, getChattingData) => {
+  console.log('sessionId shipporrrrrrrr>>>', sessionId);
+  const socket = new SockJs(`${process.env.REACT_APP_SERVER_URL}/ws-stomp`);
   stompClient = Stomp.over(() => socket);
   stompClient.connect(
     {
-      headers: {
-        Access_Token: getCookie('AccessToken'),
-      },
+      Authorization: `Bearer ${accessToken}`,
+      Refresh_Token: `Bearer ${refreshToken}`,
     },
-    function (frame) {
+    (frame) => {
       console.log('frame====> 연결 성공!', frame);
+      stompClient.subscribe(`/sub/chat/room/${sessionId}`, getChattingData);
+      // stompClient.send(`/pub/chat/message`);
     }
   );
 };
 
-export { connectClient };
+const sendMessage = async (payload) => {
+  const message = {
+    sessionId: payload.sessionId,
+    time: payload.time,
+    profile: payload.profile,
+    nickname: payload.nicnakme,
+    message: payload.message,
+  };
+  await stompClient.send(`/pub/chat/message`, message);
+};
+
+export { connectClient, sendMessage };
