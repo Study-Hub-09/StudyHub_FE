ea0ed845a8b89e5bcc60433aaf9f504848be2fe2
Hanbit Kang <hanbitkang95@gmail.com>
2023-06-30 15:05:42 +0900

    feat: PageNotFound for unexisting requested URL

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import empty from '../assets/Images/emtpy.png';
+import {
+  StPageNotFoundBox,
+  StPageNotFoundContainer,
+  StPageNotFoundHeader,
+  StPageNotFoundContent,
+  StLinkStudyHub,
+} from '../styles/Common.styles';
+import { Link } from 'react-router-dom';
 
 function PageNotFound() {
-  return <div>PageNotFound</div>;
+  return (
+    <StPageNotFoundContainer>
+      <StPageNotFoundBox>
+        <StPageNotFoundHeader>
+          <img src={empty} alt="" />
+        </StPageNotFoundHeader>
+        <StPageNotFoundContent>
+          <h1>404</h1>
+          <h2>Page Not Found</h2>
+          <p>
+            The requested URL was not found on this server. Go back to
+            <StLinkStudyHub to="/">StudyHub</StLinkStudyHub>.
+          </p>
+        </StPageNotFoundContent>
+      </StPageNotFoundBox>
+    </StPageNotFoundContainer>
+  );
 }
 
 export default PageNotFound;
